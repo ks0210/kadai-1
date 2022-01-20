99fdfdcbe43618d7c45c04ca81ff810362c085d1
ks0210 <ks0210@users.noreply.github.com>
2022-01-20 01:26:33 +0000

    課題3を追加

--- kadai-03/index3.js
+++ kadai-03/index3.js
@@ -17,10 +17,10 @@ for (let n = 2; n <= 10000; n = n + 1) {
         if (n % 3 === 1) {
             count_3n1 = count_3n1 + 1
         } else if (n % 3 === 2) {
-            count_3n2 + 1
+            count_3n2 =count_3n2 + 1
         }
     }
 }
 
 console.log('3で割ったら1余る素数の数：'　+ count_3n1)
-console.log('3で割ったら2余る素数の数：' + count_3n2)
+console.log('3で割ったら2余る素数の数：'  + count_3n2)

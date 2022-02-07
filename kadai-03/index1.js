c3a26f2ee60d1d15874cb254d2f0825806d90aae
Kamata, Ryo <mugil.cephalus+github@gmail.com>
2022-02-07 19:28:54 +0900

    Update index1.js
    インクリメント（数字型の変数に1を足す）書き方が間違っていました。

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -1,7 +1,7 @@
 const n = parseInt(process.argv[2])
 
 let isPrime = true
-for (let i = 2; i <= n -1; i = + 1) {
+for (let i = 2; i <= n -1; i = i + 1) {
     if(n % i === 0) {
         // console.log('素数でない')
         isPrime = false
